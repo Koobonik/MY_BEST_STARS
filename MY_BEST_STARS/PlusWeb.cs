6bc509fe9e8d87765c6660d217b7a269ac83ead7
Koobonik <kbi960130@naver.com>
2018-05-17 13:49:58 +0900

    버튼 여러개 생성을 위한 기반 마련

--- MY_BEST_STARS/PlusWeb.cs
+++ MY_BEST_STARS/PlusWeb.cs
@@ -29,27 +29,26 @@ namespace MY_BEST_STARS
             /*
             User user = new User(namebox.Text);//name박스의 텍스트를 불러와서 이름을 넣어줌
             */
+            int i = 1;
             MakeWeb makeweb = new MakeWeb(textBox1.Text, textBox2.Text);//이름과 주소를 받아와서 웹을 만듦
 
-            Button btn = new Button();
 
-            btn.Name = "btn" + 1;
+            while (true)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(i*40, 30);
+                btn.Name = "btn" + i;
 
-            btn.Text = btn.Name;
-
-            btn.Size = new Size(80, 60);
-
-            btn.Location = new Point(10 + 1 * (80 + 5), 50);
-            MainGUI form2 = new MainGUI();
-            form2.Controls.Add(btn);
-
-            btn.Visible = true;
-            this.Visible = false;//창닫고
-            form2.ShowDialog();//그 창을 띄움
-
-
-
-
+                btn.Text = btn.Name;
+                btn.Location = new Point(10 + i * (80 + 5), 50);
+                MainGUI form2 = new MainGUI();
+                form2.Controls.Add(btn);
+                ++i;
+                btn.Visible = true;
+                this.Visible = false;//창닫고
+                form2.ShowDialog();//그 창을 띄움
+                break;
+            }
         }
 
         private void Cancel_Click(object sender, EventArgs e)
