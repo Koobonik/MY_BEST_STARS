649eea645398a8d96e806010724ad13934931e31
Koobonik <kbi960130@naver.com>
2018-05-21 10:49:01 +0900

    btn과 form 만드는 코드를 전역선언

--- MY_BEST_STARS/PlusWeb.cs
+++ MY_BEST_STARS/PlusWeb.cs
@@ -12,6 +12,9 @@ namespace MY_BEST_STARS
 {
     public partial class PlusWeb : Form
     {
+        Button btn = new Button();//버튼 객체생성
+        MainGUI form2 = new MainGUI();//MainGUI로 폼을 만들고
+        int i = 1;
         public PlusWeb()
         {
             InitializeComponent();
@@ -26,17 +29,13 @@ namespace MY_BEST_STARS
         }
         private void Web_Enter_Click(object sender, EventArgs e)//웹 등록 버튼
         {
-            int i = 1;
             while (true)
             {
-
-                Button btn = new Button();//버튼 객체생성
                 btn.Size = new Size(80, 30);//버튼 사이즈
                 btn.Text = textBox1.Text;//이름은 텍스트 박스 1 에서 가져옴
                 btn.Click += new EventHandler(btn_Click);//버튼 클릭 기능추가
                 btn.Location = new Point(10 + i * 100, 50);//버튼의 위치 ( 가로, 세로 )
-                btn.Visible = true;
-                MainGUI form2 = new MainGUI();//MainGUI로 폼을 만들고
+                btn.Visible = true;//btn 있다고 하기
                 form2.Controls.Add(btn);//form2 에 버튼을 추가한다.
                 ++i;
                 this.Visible = false;//창닫고
