08ff0bd283b7bf854e0e09d350befa1fd001287d
Koobonik <kbi960130@naver.com>
2018-05-24 00:03:36 +0900

    다른 추가 버튼

---
+++ MY_BEST_STARS/application.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MY_BEST_STARS
+{
+    partial class application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cal
+            // 
+            this.cal.Location = new System.Drawing.Point(31, 31);
+            this.cal.Name = "cal";
+            this.cal.Size = new System.Drawing.Size(89, 23);
+            this.cal.TabIndex = 0;
+            this.cal.Text = "계산기 추가";
+            this.cal.UseVisualStyleBackColor = true;
+            this.cal.Click += new System.EventHandler(this.cal_Click);
+            // 
+            // application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cal);
+            this.Name = "application";
+            this.Text = "응용프로그램";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cal;
+    }
+}
---
+++ MY_BEST_STARS/application.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MY_BEST_STARS
+{
+    public partial class application : Form
+    {
+        public application()
+        {
+            InitializeComponent();
+        }
+
+        private void cal_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("C:\Windows\System32\cale.exe");
+        }
+    }
+}
